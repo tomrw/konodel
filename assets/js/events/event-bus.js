af5eab8fdc1196477d36ac2f8e97060d5e8b415d
Tom West <westendpro@gmail.com>
2014-04-28 21:45:01 +0100

    Add event bus

---
+++ assets/js/events/event-bus.js
@@ -0,0 +1,39 @@
+define(function() {
+
+	function EventBus() {
+		this._listeners = {};
+	}
+
+	var EventBusPrototype = EventBus.prototype;
+
+	EventBusPrototype.on = function(event, callback) {
+		if (!this._listeners[event]) {
+			this._listeners[event] = [];
+		}
+		this._listeners[event].push(callback);
+	};
+
+	EventBusPrototype.off = function(event, callback) {
+		var listeners = this._listeners[event];
+		if (listeners && callback) {
+			for (var i = listeners.length; i >= 0; --i) {
+				var listener = listeners[i];
+				if (listener === callback) {
+					listeners.splice(i, 1);
+				}
+			}
+		}
+		else {
+			delete this._listeners[event];
+		}
+	}
+
+	EventBusPrototype.trigger = function(event) {
+		var listeners = this._listeners[event] || [];
+		for (var i = 0, l = listeners.length; i < l; ++i) {
+			listeners[i].apply(null, arguments);
+		}
+	};
+
+	return EventBus;
+});
